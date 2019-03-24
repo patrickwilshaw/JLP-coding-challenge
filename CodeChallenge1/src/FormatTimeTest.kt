117dc2f469c8e97f48b5fed4ead5d913fc428f56
patrickwilshaw <48862470+patrickwilshaw@users.noreply.github.com>
2019-03-24 15:36:55 +0000

    Update FormatTimeTest.kt

--- CodeChallenge1/src/FormatTimeTest.kt
+++ CodeChallenge1/src/FormatTimeTest.kt
@@ -8,7 +8,22 @@
 //    }
 //
 //    @Test
+//    fun 'Check that 1 second returns 1 second'() {
+//        assertEquals(formatTime(1), "1 second")
+//    }
+//
+//    @Test
+//    fun 'Check that 62 seconds returns 1 minute and 2 seconds'() {
+//        assertEquals(formatTime(1), "1 minute and 2 seconds")
+//    }
+//
+//    @Test
 //    fun 'Check that 94609440 seconds is 3 years and 24 minutes'() {
+//        assertEquals(formatTime(94609440), "3 years and 24 minutes")
+//    }
+//
+//    @Test
+//    fun 'Check that 94609440 seconds is 3 days, 4 hours and 1 minute'() {
 //        assertEquals(formatTime(94609440), "3 years and 24 minutes")
 //    }
 //}
