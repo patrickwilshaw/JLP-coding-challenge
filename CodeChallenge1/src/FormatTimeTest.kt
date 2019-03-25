c4ccb11fae57320582777b4d48b62f05374f0e59
patrickwilshaw <48862470+patrickwilshaw@users.noreply.github.com>
2019-03-25 15:59:06 +0000

    Update FormatTimeTest.kt

--- CodeChallenge1/src/FormatTimeTest.kt
+++ CodeChallenge1/src/FormatTimeTest.kt
@@ -4,26 +4,26 @@ import kotlin.test.assertEquals
 internal class FormatTimeTest {
     @Test
     fun `check that none is returned when seconds are 0`() {
-        assertEquals(formatTime(0), "none")
+        assertEquals("none",formatTime(0))
     }
 
     @Test
     fun `check that 1 second returns 1 second`() {
-        assertEquals(formatTime(1), "1 second")
+        assertEquals( "1 second",formatTime(1))
     }
 
     @Test
     fun `check that 62 seconds returns 1 minute and 2 seconds`() {
-        assertEquals(formatTime(62), "1 minute and 2 seconds")
+        assertEquals( "1 minute and 2 seconds",formatTime(62))
     }
 
     @Test
     fun `check that 94609440 seconds is 3 years and 24 minutes`() {
-        assertEquals(formatTime(94609440), "3 years and 24 minutes")
+        assertEquals("3 years and 24 minutes",formatTime(94609440) )
     }
 
     @Test
     fun `check that 94609440 seconds is 3 days, 4 hours and 1 minute`() {
-        assertEquals(formatTime(94609440), "3 years and 24 minutes")
+        assertEquals("3 years, 4 hours and 24 minutes",formatTime(94623840))
     }
 }
