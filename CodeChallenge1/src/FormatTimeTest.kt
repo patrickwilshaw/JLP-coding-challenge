da4e924e0ad8738bd2df7bc486a2a986b8808bb0
patrickwilshaw <48862470+patrickwilshaw@users.noreply.github.com>
2019-03-25 12:19:09 +0000

    Update FormatTimeTest.kt

--- CodeChallenge1/src/FormatTimeTest.kt
+++ CodeChallenge1/src/FormatTimeTest.kt
@@ -1,30 +1,29 @@
-//import org.junit.Test
-//import kotlin.test.assertEquals
-//
-//internal class FormatTimeTest {
-//    @Test
-//    fun 'Check that none is returned when seconds are 0'() {
-//        assertEquals(formatTime(0), "none")
-//    }
-//
-//    @Test
-//    fun 'Check that 1 second returns 1 second'() {
-//        assertEquals(formatTime(1), "1 second")
-//    }
-//
-//    @Test
-//    fun 'Check that 62 seconds returns 1 minute and 2 seconds'() {
-//        assertEquals(formatTime(1), "1 minute and 2 seconds")
-//    }
-//
-//    @Test
-//    fun 'Check that 94609440 seconds is 3 years and 24 minutes'() {
-//        assertEquals(formatTime(94609440), "3 years and 24 minutes")
-//    }
-//
-//    @Test
-//    fun 'Check that 94609440 seconds is 3 days, 4 hours and 1 minute'() {
-//        assertEquals(formatTime(94609440), "3 years and 24 minutes")
-//    }
-//}
-//
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class FormatTimeTest {
+    @Test
+    fun `Check that none is returned when seconds are 0`() {
+        assertEquals(formatTime(0), "none")
+    }
+
+    @Test
+    fun `Check that 1 second returns 1 second`() {
+        assertEquals(formatTime(1), "1 second")
+    }
+
+    @Test
+    fun `Check that 62 seconds returns 1 minute and 2 seconds`() {
+        assertEquals(formatTime(62), "1 minute and 2 seconds")
+    }
+
+    @Test
+    fun `Check that 94609440 seconds is 3 years and 24 minutes`() {
+        assertEquals(formatTime(94609440), "3 years and 24 minutes")
+    }
+
+    @Test
+    fun `Check that 94609440 seconds is 3 days, 4 hours and 1 minute`() {
+        assertEquals(formatTime(94609440), "3 years and 24 minutes")
+    }
+}
