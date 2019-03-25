4ea9b1dbb76925398a07f0383d84cece3aee4098
patrickwilshaw <48862470+patrickwilshaw@users.noreply.github.com>
2019-03-25 15:58:36 +0000

    Update FormatTime.kt

--- CodeChallenge1/src/FormatTime.kt
+++ CodeChallenge1/src/FormatTime.kt
@@ -6,7 +6,7 @@ fun formatTime(seconds: Int = 0): String {
     if (seconds == 0) {
         return "none"
     }
-   
+
     var remainder = seconds
     var formattedTime = ""
     for (unitTime in TimeUnit.values()) {
@@ -16,15 +16,7 @@ fun formatTime(seconds: Int = 0): String {
             formattedTime += formatUnit(noUnits, unitTime)
         }
     }
-    //Remove the last comma and change the penultimate comma if applicable to an 'and' finally put the space back after commas
-    var position = formattedTime.lastIndexOf(",")
-    if (position != -1) {
-        formattedTime = formattedTime.replaceRange(position, position + 1, "")
-        position = formattedTime.lastIndexOf(",")
-        if (position != -1) {
-            formattedTime = formattedTime.replaceRange(position, position + 1, " and ")
-        }
-    }
+    formattedTime = formatTimeString(formattedTime)
     return formattedTime.replace(",", ", ");
 }
 
@@ -37,6 +29,20 @@ fun formatUnit(noUnits: Int, Unit: TimeUnit): String {
     } else {
         return noUnits.toString() + " " + Unit.unitDescSingular + ","
     }
+}
+
+fun formatTimeString(informattedTime: String) : String {
+    //Remove the last comma and change the penultimate comma if applicable to an 'and' finally put the space back after commas
+    var formattedTime=inFormattedTime
+    var position = formattedTime.lastIndexOf(",")
+    if (position != -1) {
+        formattedTime = formattedTime.replaceRange(position, position + 1, "")
+        position = formattedTime.lastIndexOf(",")
+        if (position != -1) {
+            formattedTime = formattedTime.replaceRange(position, position + 1, " and ")
+        }
+    }
+    return formattedTime
 }
 
 enum class TimeUnit(val noSeconds: Int, val unitDescSingular: String, val unitDescPlural: String) {
