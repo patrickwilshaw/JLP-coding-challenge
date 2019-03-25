59c9186a03f9d716c1131152525eabb61412750e
patrickwilshaw <48862470+patrickwilshaw@users.noreply.github.com>
2019-03-25 12:24:16 +0000

    change function names to lower case

--- CodeChallenge1/src/FormatTimeTest.kt
+++ CodeChallenge1/src/FormatTimeTest.kt
@@ -3,27 +3,27 @@ import kotlin.test.assertEquals
 
 internal class FormatTimeTest {
     @Test
-    fun `Check that none is returned when seconds are 0`() {
+    fun `check that none is returned when seconds are 0`() {
         assertEquals(formatTime(0), "none")
     }
 
     @Test
-    fun `Check that 1 second returns 1 second`() {
+    fun `check that 1 second returns 1 second`() {
         assertEquals(formatTime(1), "1 second")
     }
 
     @Test
-    fun `Check that 62 seconds returns 1 minute and 2 seconds`() {
+    fun `check that 62 seconds returns 1 minute and 2 seconds`() {
         assertEquals(formatTime(62), "1 minute and 2 seconds")
     }
 
     @Test
-    fun `Check that 94609440 seconds is 3 years and 24 minutes`() {
+    fun `check that 94609440 seconds is 3 years and 24 minutes`() {
         assertEquals(formatTime(94609440), "3 years and 24 minutes")
     }
 
     @Test
-    fun `Check that 94609440 seconds is 3 days, 4 hours and 1 minute`() {
+    fun `check that 94609440 seconds is 3 days, 4 hours and 1 minute`() {
         assertEquals(formatTime(94609440), "3 years and 24 minutes")
     }
 }
